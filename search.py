7b0a7719fe89a77da91138d9ce027adc3d3f26b4
Lynn Wang <lynncwang@gmail.com>
2015-11-14 21:59:18 -0500

    initial commit

---
+++ search.py
@@ -0,0 +1,48 @@
+from flask import Flask, send_from_directory, request, render_template
+import requests
+import json
+from emotion_subject import emotion_subject_dict
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return send_from_directory('templates','index.html')
+
+def createUrl(subjects):
+    query = '+'.join(subjects)
+    full_query = "http://openlibrary.org/search.json?subject="+query
+    return full_query
+
+@app.route("/query", methods= ['POST'])
+def searchUrl():
+    try:
+        user_emotion = request.form['emotion']
+        user_subjects = emotion_subject_dict[user_emotion]
+        full_query = createUrl(user_subjects)
+        resp = requests.get(full_query)
+        data = json.loads(resp.text)
+        books = []
+
+        for i in range(29):
+            book = {}
+            if 'title_suggest' in data['docs'][i]:
+                book['title'] = data['docs'][i]['title_suggest']
+            if 'author_name' in data['docs'][i]:
+                book['author'] = data['docs'][i]['author_name'][0]
+            else: book['author'] = 'Author unknown'
+            if 'cover_i' in data['docs'][i]:
+                book['cover_i']=data['docs'][i]['cover_i']
+            else: book['cover_i']=0
+            books.append(book)
+        print books
+        return render_template('results.html', books=books)
+
+    except Exception as e:
+        print e
+        
+@app.route("/about")
+def hello():
+    return "hello world!"
+
+if __name__ == "__main__":
+    app.run()
